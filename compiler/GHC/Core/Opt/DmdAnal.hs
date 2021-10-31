a7e99e46076521148cc73c144407bba710e696e5
Joachim Breitner <mail@joachim-breitner.de>
2021-10-31 10:55:42 +0100

    Remove assert in DmdAnal

--- compiler/GHC/Core/Opt/DmdAnal.hs
+++ compiler/GHC/Core/Opt/DmdAnal.hs
@@ -349,8 +349,9 @@ dmdAnalStar env (n :* sd) e
   -- NB: (:*) expands AbsDmd and BotDmd as needed
   -- See Note [Analysing with absent demand]
   | WithDmdType dmd_ty e' <- dmdAnal env sd e
-  = assertPpr (not (isUnliftedType (exprType e)) || exprOkForSpeculation e) (ppr e)
+  = -- assertPpr (not (isUnliftedType (exprType e)) || exprOkForSpeculation e) (ppr e)
     -- The argument 'e' should satisfy the let/app invariant
+    -- TODO: Why is this invariant needed here?
     (toPlusDmdArg $ multDmdType n dmd_ty, e')
 
 -- Main Demand Analsysis machinery
