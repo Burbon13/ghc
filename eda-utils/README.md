f2ee7b90af950c6e1489084539f9c0a097912f54
Razvan Roatis <razvanroatis91@gmail.com>
2022-03-09 11:46:41 +0100

    Refactored shell utils + added README.md

---
+++ eda-utils/README.md
@@ -0,0 +1,14 @@
+# Utilities Coherent Explicit Dictionary Applications
+
+### Shell Utils
+
+Contains the script `gen-dump.sh` which will run a Haskell program and output all the debug data.
+It will also generate an HTML report showing what the compiler does with the program step by step.
+
+### Tests
+
+Contains Haskell programs which must compile and run without any errors with the newly added GHC features.
+
+### Sample Code
+
+Various Haskell programs to illustrate the new GHC features.
--- eda-utils/shell-utils/gen-dump.sh
+++ eda-utils/shell-utils/gen-dump.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+# !!! This script must be run within the "shell-utils" folder !!!
 # Check: https://downloads.haskell.org/~ghc/latest/docs/html/users_guide/debugging.html
 # Check: https://www.aosabook.org/en/ghc.html
 
 PROGRAM="$1.hs"
-TMP_LOCATION="tmp/$1/"
-GLOBAL_GHC="ghc"
+BASE=$(basename "$1")
+TMP_LOCATION="tmp/$BASE/"
 CUSTOM_GHC="../../inplace/bin/ghc-stage2"
 
 GHC=$CUSTOM_GHC
-#GHC=$GLOBAL_GHC
+#GHC="ghc" # Comment this out to use the Global GHC
 
-rm -r $TMP_LOCATION
+rm -r "$TMP_LOCATION"
 
-$GHC -XDuplicateRecordFields -O2 -ddump-to-file -ddump-file-prefix="$TMP_LOCATION$1." \
+$GHC -XDuplicateRecordFields \
+  -O2 \
+  -ddump-to-file \
+  -ddump-file-prefix="$TMP_LOCATION$BASE." \
   -ddump-tc-trace \
   -ddump-parsed-ast -ddump-parsed \
   -ddump-rn-stats -ddump-rn -ddump-rn-ast \
@@ -22,22 +26,12 @@ $GHC -XDuplicateRecordFields -O2 -ddump-to-file -ddump-file-prefix="$TMP_LOCATIO
   -ddump-prep \
   $PROGRAM
 
-# -dsuppress-idinfo -dsuppress-coercions -dsuppress-type-applications -dsuppress-uniques -dsuppress-module-prefixes \
-
 PrintFile() {
   STAGE_NAME=$1
   IN_FILE=$2
 
   echo "<div>" >>$HTML_DUMP_FILE
   echo "<h3>$STAGE_NAME</h3>" >>$HTML_DUMP_FILE
-
-  # Check: http://hilite.me/api
-  # CODE=$(cat $IN_FILE)
-  # MODIFIED_CODE=${CODE/"'GHC.Types.LiftedRep"/"apostrofGHC.Types.LiftedRep"}
-  # curl -d "code=$MODIFIED_CODE&lexer=haskell&linenos=true" -X POST http://hilite.me/api > "$IN_FILE.pretty"
-  # PRETTY_CODE=$(cat $IN_FILE.pretty)
-  # UNMODIFIED_CODE=${PRETTY_CODE/"apostrofGHC"/"'GHC"}
-  # cat <<< "$UNMODIFIED_CODE" >> $HTML_DUMP_FILE
 
   echo "<code><div style=\"border:1px solid black; white-space: pre-wrap\">" >>$HTML_DUMP_FILE
   cat $IN_FILE >>$HTML_DUMP_FILE
@@ -48,16 +42,15 @@ PrintFile() {
 
 echo "Generating HTML file"
 
-HTML_DUMP_FILE="$TMP_LOCATION$1-dump.html"
-PARSER_OUT_FILE="$TMP_LOCATION$1.dump-parsed"
-RENAME_OUT_FILE="$TMP_LOCATION$1.dump-rn"
-TYPECHECK_OUT_FILE="$TMP_LOCATION$1.dump-tc"
-TYPECHECK_SIGNATURES_OUT_FILE="$TMP_LOCATION$1.dump-types"
-DESUGAR_BEFORE_OPT_OUT_FILE="$TMP_LOCATION$1.dump-ds-preopt"
-DESUGAR_AFTER_OPT_OUT_FILE="$TMP_LOCATION$1.dump-ds"
-CORE_2_CORE_OUT_FILE="$TMP_LOCATION$1.verbose-core2core"
-TIDY_CORE_OUT_FILE="$TMP_LOCATION$1.dump-simpl"
-CORE_PREP_OUT_FILE="$TMP_LOCATION$1.dump-prep"
+HTML_DUMP_FILE="$TMP_LOCATION$BASE-dump.html"
+PARSER_OUT_FILE="$TMP_LOCATION$BASE.dump-parsed"
+RENAME_OUT_FILE="$TMP_LOCATION$BASE.dump-rn"
+TYPECHECK_OUT_FILE="$TMP_LOCATION$BASE.dump-tc"
+TYPECHECK_SIGNATURES_OUT_FILE="$TMP_LOCATION$BASE.dump-types"
+DESUGAR_BEFORE_OPT_OUT_FILE="$TMP_LOCATION$BASE.dump-ds-preopt"
+DESUGAR_AFTER_OPT_OUT_FILE="$TMP_LOCATION$BASE.dump-ds"
+TIDY_CORE_OUT_FILE="$TMP_LOCATION$BASE.dump-simpl"
+CORE_PREP_OUT_FILE="$TMP_LOCATION$BASE.dump-prep"
 
 echo "<html>" >$HTML_DUMP_FILE
 echo "<body>" >>$HTML_DUMP_FILE
@@ -71,7 +64,6 @@ PrintFile "Typecheck" $TYPECHECK_OUT_FILE
 PrintFile "Type Signatures" $TYPECHECK_SIGNATURES_OUT_FILE
 PrintFile "Desugar (before optimization)" $DESUGAR_BEFORE_OPT_OUT_FILE
 PrintFile "Desugar (after optimization)" $DESUGAR_AFTER_OPT_OUT_FILE
-PrintFile "Core2Core Verbose" $CORE_2_CORE_OUT_FILE
 PrintFile "Tidy core" $TIDY_CORE_OUT_FILE
 PrintFile "Core prep" $CORE_PREP_OUT_FILE
 
@@ -79,7 +71,6 @@ echo "</body>" >>$HTML_DUMP_FILE
 echo "</html>" >>$HTML_DUMP_FILE
 
 rm "$1".o "$1".hi
-# rm "$1" "$1".o "$1".hi
 
 INDEX_HTML="index.html"
 
